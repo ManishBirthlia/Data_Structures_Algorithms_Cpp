546b41a0de0bd369c57aa653e467733f6ea3651d
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-07-16 18:21:09 +0530

    Add files via upload

---
+++ 1st_intro.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // printf("Hello world my name is manish and what about you");
+    cout<<"Hello world my name is manish and what about you";
+    return 0;
+}
