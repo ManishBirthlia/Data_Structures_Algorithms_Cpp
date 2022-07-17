9b2ec6aea7afc2d9cd37d80ae4742708e68b201a
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-07-18 00:23:44 +0530

    Add files via upload

---
+++ 3rd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int sum(float a,float b){
+    return a+b;
+}
+int main(){
+    // int a,b;
+    // cout<<"take any value of a"<<endl;
+    // cin>>a;
+    // cout<<"take any value of b"<<endl;
+    // cin>>b;
+    cout<<"sum of a and b is "<<sum(45,45.5);
+
+}
