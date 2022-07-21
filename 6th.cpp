76c74b0e2bae5784297352cf4fcf69ae56aa34b3
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-07-21 12:00:30 +0530

    Add files via upload

---
+++ 6th.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char a[7];
+    // cin>>a;
+    // for(int i=0;i<2;i++){
+    //     cout<<a[i];
+    // }
+    cout<<endl;
+    cout<<a;
+}
