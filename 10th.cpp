7ce1706b7ec6fdbdf633ec8ce4c628bdfce5cb0c
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-07-27 23:44:11 +0530

    Add files via upload

---
+++ 10th.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n=123;
+    for(int i=1;i<n;i++){
+        if (n/pow(10,i)<1)
+        {
+            break;
+        }
+        for(int j=1;j<n;j++){
+            if()
+        cout<<fmod(n, pow(10,i))<<endl;
+        }
+    }
+    // cout<<"this is "<<s<<endl;
+	return 0;
+}
