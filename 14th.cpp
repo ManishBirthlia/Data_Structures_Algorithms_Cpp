a048a4994302b25c5be900aad6a992dcf4ebdd50
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-08-04 00:20:15 +0530

    Add files via upload

---
+++ 14th.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a%5<<" "<<b%5<<" "<<(a-b)/5<<endl;
+    //cout<<a/5<<" "<<b/5<<" "<<(a-b)/5-1<<endl;
+}
