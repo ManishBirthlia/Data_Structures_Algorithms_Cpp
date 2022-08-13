eaa602e35d3e020767564a0a693192d4ace1595c
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-08-13 11:27:28 +0530

    Add files via upload

---
+++ Leetcode_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<"a+b is equal to"<<a+b<<endl;
+    cout<<"a-b is equal to"<<a-b<<endl;
+    cout<<"a*b is equal to"<<a*b<<endl;
+    cout<<"a/b is equal to"<<a/b<<endl;
+    cout<<"a%b is equal to"<<a%b<<endl;
+}
