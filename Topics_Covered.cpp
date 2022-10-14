b0f7cf55900a4cc32e6251a4b2205be9d02ba905
Manish Birthlia <96776615+ManishBirthlia@users.noreply.github.com>
2022-10-14 15:19:49 +0530

    Add files via upload

---
+++ Topics_Covered.cpp
@@ -0,0 +1,22 @@
+// Topics i have covered in C++ are:-
+// Data types  (int, float, double, string, char)
+// Array in   (int, char, string, float)
+        // int array and char array are work differently
+// reference veriable :-
+        // int &a=b;
+// Vector properties:-
+        // vector<datatype> name(size,element);                                   // this is to create vector staticlly
+        // vector<datatype> *name=new vector<datatype>(size,element);             // this is to create vector dynamically
+        // push_back(element);                                                    // function
+        // at(index);
+        // capacity();
+        // pop_back();
+// Sort:- 
+        // Selection Sort
+        // bubble Sort
+        // optimized bubble sort (for already sorted array)
+        // Merge Sort
+        // Quick Sort
+// search function:- (function to find index at which key is present)
+        // lianear search
+        // binary search (its for sorted array)
